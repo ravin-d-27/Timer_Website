f8442912530ac77daf2a6ba920ac200b26928836
Ravin D <ravin.d3107@outlook.com>
2023-11-29 18:28:30 +0530

    Test2

--- Timerweb/build_files.sh
+++ Timerweb/build_files.sh
@@ -1,4 +1,4 @@
  echo "BUILD START"
- python -m pip install -r requirements.txt
- python manage.py collectstatic
+ python3 -m pip install -r requirements.txt
+ python3 manage.py collectstatic
  echo "BUILD END"
