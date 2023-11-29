10b66d9f60c8a5f2c765758881bad8a01ce81064
Ravin D <ravin.d3107@outlook.com>
2023-11-29 21:04:48 +0530

    Added SQLite database itself

--- Timerweb/Timerweb/settings.py
+++ Timerweb/Timerweb/settings.py
@@ -78,9 +78,11 @@ WSGI_APPLICATION = 'Timerweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://toastmasters_timerweb_user:2h8LcUxZw0mowDumdrPYQWUe0refHQYT@dpg-cljdnhlae00c7385kj9g-a.singapore-postgres.render.com/toastmasters_timerweb')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
