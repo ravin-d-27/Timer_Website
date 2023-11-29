321c8a413fbb6b3b861cb3de71db53f38378cc97
Ravin D <ravin.d3107@outlook.com>
2023-11-29 18:51:52 +0530

    Test7

--- Timerweb/build_files.sh
+++ Timerweb/build_files.sh
@@ -1,5 +1,4 @@
 echo "BUILD START"
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput 
-echo Y | icacls Timer\Data_time\timer_data.csv /grant:r Everyone:(R,W)
 echo "BUILD END"
