c61cbf0049dc32e4684a5c0091aef95cf8debdc4
Ravin D <ravin.d3107@outlook.com>
2023-11-29 18:11:07 +0530

    Attached PostgresSQL database

--- Timerweb/Timerweb/settings.py
+++ Timerweb/Timerweb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'Timerweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://toastmasters_timerweb_user:2h8LcUxZw0mowDumdrPYQWUe0refHQYT@dpg-cljdnhlae00c7385kj9g-a.singapore-postgres.render.com/toastmasters_timerweb')
 }
 
 
