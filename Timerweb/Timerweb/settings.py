a9341cf1172c390283844006a47f95c0196945af
Ravin D <ravin.d3107@outlook.com>
2023-11-30 11:35:49 +0530

    Changed the staticfiles dirs according to production

--- Timerweb/Timerweb/settings.py
+++ Timerweb/Timerweb/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR,'TimerWeb/static/')
+os.path.join(BASE_DIR,'/home/RavinD27Toastmasters/Timer_Website/Timerweb/static')
 ]
 
 STATIC_URL = '/static/'
