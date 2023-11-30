163ca10b57f873d3c32913358d3e78a54e46a48a
Ravin D <ravin.d3107@outlook.com>
2023-11-30 11:38:51 +0530

    Changed the staticfiles dirs according to production

--- Timerweb/Timerweb/settings.py
+++ Timerweb/Timerweb/settings.py
@@ -119,13 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR,'/home/RavinD27Toastmasters/Timer_Website/Timerweb/static')
+    os.path.join(BASE_DIR, 'home', 'RavinD27Toastmasters', 'Timer_Website', 'Timerweb', 'static'),
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_URL ='/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
